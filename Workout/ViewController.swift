34d6f2c8fb25896e3548e761fe6b3859a1c468f8
Donovan <donovan.cotter@detroitlabs.com>
2018-12-21 09:54:31 -0500

    Initial Commit

---
+++ Workout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Workout
+//
+//  Created by Donovan Cotter on 12/21/18.
+//  Copyright © 2018 Donovan Cotter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
